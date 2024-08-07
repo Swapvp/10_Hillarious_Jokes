6dd4aef19d0a8a8e8d02457a26e6a659a4444a20
Swapvp <swapneshpawar1994@gmail.com>
2024-08-07 17:42:30 +0530

    jokes

--- script.js
+++ script.js
@@ -6,31 +6,31 @@ jokeBtn.addEventListener("click", generateJoke);
 generateJoke();
 
 // USING ASYNC/AWAIT
-async function generateJoke() {
-  const config = {
-    headers: {
-      Accept: "application/json",
-    },
-  };
-
-  const res = await fetch("https://icanhazdadjoke.com", config);
-
-  const data = await res.json();
-
-  jokeEl.innerHTML = data.joke;
-}
-
-// USING .then()
-// function generateJoke() {
+// async function generateJoke() {
 //   const config = {
 //     headers: {
-//       Accept: 'application/json',
+//       Accept: "application/json",
 //     },
-//   }
+//   };
 
-//   fetch('https://icanhazdadjoke.com', config)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       jokeEl.innerHTML = data.joke
-//     })
+//   const res = await fetch("https://icanhazdadjoke.com", config);
+
+//   const data = await res.json();
+
+//   jokeEl.innerHTML = data.joke;
 // }
+
+USING .then()
+function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  fetch('https://icanhazdadjoke.com', config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke
+    })
+}
